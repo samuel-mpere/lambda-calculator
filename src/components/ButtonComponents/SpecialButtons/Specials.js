ea478d1f0dcbb50b31506f92ed476317303b25d6
chudnofskyATD <mpereannor@gmail.com>
2019-09-13 06:29:02 +0000

    Operators completed

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -7,10 +7,10 @@ import SpecialButton from './SpecialButton';
 
 //Import your array data to from the provided data file
 
-const Specials = (props) => {
+const Specials = () => {
   // STEP 2 - add the imported data to state
 
-  const[specialsState, setSpecialsState] = useState
+  const[specialsState, setSpecialsState] = useState(specials);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
